35798f2e4c05451c4f22fd4d92d34f9c8daef632
TechBree <bridget_kodohu@yahoo.com>
2024-12-09 21:59:25 +0100

    Created a function for option

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("Hello World");
+function showTimezone(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "paris") {
+      alert(`It is ${firstCountry}`);
+    }
+    if (event.target.value === "tokyo") {
+      alert(`It is ${secondCountry}`);
+    }
+    if (event.target.value === "sydney") {
+      alert(`It is ${thirdCountry}`);
+    }
+  }
+}
+
+let firstCountry = moment()
+  .tz("Europe/Paris")
+  .format("dddd, MMMM Do, YYYY HH:MM A");
+
+let secondCountry = moment()
+  .tz("Asia/Tokyo")
+  .format("dddd, MMMM Do, YYYY HH:MM A");
+
+let thirdCountry = moment()
+  .tz("Australia/Sydney")
+  .format("dddd, MMMM Do, YYYY HH:MM A");
+
+let countryElement = document.querySelector("#countries");
+countryElement.addEventListener("change", showTimezone);
